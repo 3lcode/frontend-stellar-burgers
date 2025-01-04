7e553fc1f9cee8d6893b0b12fbe7404786d4fcd4
3lcode <3lcode@mail.ru>
2025-01-04 22:47:46 +0300

    сделан проект

--- src/components/burger-constructor-element/burger-constructor-element.tsx
+++ src/components/burger-constructor-element/burger-constructor-element.tsx
@@ -1,14 +1,24 @@
 import { FC, memo } from 'react';
 import { BurgerConstructorElementUI } from '@ui';
 import { BurgerConstructorElementProps } from './type';
+import { AppDispatch, useDispatch } from '../../services/store';
+import { moveIngredient, removeIngredient } from '../../services/slices/burgerConstructorSlice';
 
 export const BurgerConstructorElement: FC<BurgerConstructorElementProps> = memo(
   ({ ingredient, index, totalItems }) => {
-    const handleMoveDown = () => {};
+    const dispatch: AppDispatch = useDispatch();
 
-    const handleMoveUp = () => {};
+    const handleMoveDown = () => {
+      dispatch(moveIngredient({type: 'down', index}));
+    };
 
-    const handleClose = () => {};
+    const handleMoveUp = () => {
+      dispatch(moveIngredient({type: 'up', index}));
+    };
+
+    const handleClose = () => {
+      dispatch(removeIngredient(index));
+    };
 
     return (
       <BurgerConstructorElementUI
